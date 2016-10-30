5d7ad40a89ddae710394e62baecd0e39c935eff2
Ana Paula Mello <apcomello@gmail.com>
2016-10-30 16:10:23 -0200

    Creation of main file with the basic C++ main template

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    
+    int i =0;
+    
+    cout << "Hello, world" << endl;
+    cout << "This will be a cache simulator one day." << endl;
+    
+    cin >> i;
+    
+    return 0;
+}
